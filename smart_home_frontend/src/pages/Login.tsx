a2f724b1f226cc6d6f09a8c0f56bf3d9cadda8df
pavleGasic <pavle.gasic7@gmail.com>
2024-01-30 23:02:07 +0100

    initial commit

---
+++ smart_home_frontend/src/pages/Login.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Login.scss";
+
+type Props = {};
+
+const Login = (props: Props) => {
+    return(
+        <>
+        <h1>Paja</h1>
+        </>
+    );
+};
+
+export default Login;
