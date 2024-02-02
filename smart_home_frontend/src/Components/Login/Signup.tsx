2d695e37c935d5776e2a1a24e7a88da1f0345d74
StefanJo3107 <sjovanovic0831@gmail.com>
2024-02-03 00:40:41 +0100

    Added register

--- smart_home_frontend/src/Components/Login/Signup.tsx
+++ smart_home_frontend/src/Components/Login/Signup.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Hyperlink from "../Shared/Hyperlink";
 import PrimaryButton from "../Shared/PrimaryButton";
 import TextBox from "../Shared/TextBox";
@@ -12,6 +12,8 @@ import { BiSolidCctv } from "react-icons/bi";
 import { IoIosArrowBack } from "react-icons/io";
 import "./Signup.scss";
 import { LoginPages } from "../../Pages/Login";
+import { useNavigate } from "react-router-dom";
+import { useRegister } from "../../Api/AuthApi";
 
 type Props = {
     selectedPage: LoginPages;
@@ -19,6 +21,35 @@ type Props = {
 };
 
 const Signup = (props: Props) => {
+    const [fullname, setFullname] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [locationCode, setLocationCode] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+    const [refetched, setRefetched] = useState(false);
+    const navigate = useNavigate();
+
+    const { data, refetch, isLoading, isError } = useRegister(
+        fullname.split(" ")[0],
+        fullname.split(" ")[1],
+        username,
+        password,
+        locationCode
+    );
+
+    useEffect(() => {
+        if (!isLoading && !isError) {
+            if (data != undefined && refetched) {
+                localStorage.setItem("access_token", data?.token);
+                setErrorMessage("");
+                setRefetched(false);
+                navigate("/");
+            }
+        } else if (isError) {
+            setErrorMessage("Invalid credentials!");
+        }
+    }, [data, isError]);
+
     return (
         <div className="login-container">
             <GlassDiv className="login-wrapper">
@@ -32,22 +63,51 @@ const Signup = (props: Props) => {
                     {"Sign up"}
                     <div></div>
                 </div>
-                <div className="su-firstname-txb">
-                    <TextBox placeholder="First name"></TextBox>
-                </div>
-                <div className="su-lastname-txb">
-                    <TextBox placeholder="Last name"></TextBox>
+                <div className="su-fullname-txb">
+                    <TextBox
+                        placeholder="Full name"
+                        value={fullname}
+                        onChanged={(e) => {
+                            setFullname(e.target.value);
+                        }}
+                    ></TextBox>
                 </div>
                 <div className="su-username-txb">
-                    <TextBox placeholder="Username"></TextBox>
+                    <TextBox
+                        placeholder="Username"
+                        value={username}
+                        onChanged={(e) => {
+                            setUsername(e.target.value);
+                        }}
+                    ></TextBox>
                 </div>
                 <div className="su-password-txb">
-                    <TextBox placeholder="Password"></TextBox>
+                    <TextBox
+                        placeholder="Password"
+                        value={password}
+                        onChanged={(e) => {
+                            setPassword(e.target.value);
+                        }}
+                    ></TextBox>
+                </div>
+                <div className="su-location-txb">
+                    <TextBox
+                        placeholder="Location code"
+                        value={locationCode}
+                        onChanged={(e) => {
+                            setLocationCode(e.target.value);
+                        }}
+                    ></TextBox>
                 </div>
                 <PrimaryButton
                     button_value="Sign up"
                     className="su-primary-button"
+                    onClick={() => {
+                        refetch();
+                        setRefetched(true);
+                    }}
                 ></PrimaryButton>
+                <div className="error-message">{errorMessage}</div>
                 <div className="login-icon purifier">
                     <SignInIcon icon={<MdAir />}></SignInIcon>
                 </div>
